e12c63709e56e1c76ccd6f73133968c80a3fb03f
Leonardo Cortez <leonardocortez389@gmail.com>
2022-10-10 13:00:41 -0500

    Se agrega la ventana para seleccionar archivo, falta adaptarlo al evento del botón

--- src/PruebasLeonardo/Principal.java
+++ src/PruebasLeonardo/Principal.java
@@ -1,9 +1,16 @@
 package PruebasLeonardo;
 
+import javax.swing.*;
+import java.io.File;
+
 public class Principal {
     public static void main(String[] args) {
         Ventana v1 = new Ventana(); // Objeto para la clase ventana
-
+        File archivoSeleccionado;
+        JFileChooser seleccionarArchivo;
+        seleccionarArchivo = new JFileChooser();
+        seleccionarArchivo.showOpenDialog(null);
+        archivoSeleccionado = seleccionarArchivo.getSelectedFile();
         v1.setVisible(true); // muestra la ventana
     }
 }
--- src/PruebasLeonardo/Ventana.java
+++ src/PruebasLeonardo/Ventana.java
@@ -1,5 +1,8 @@
 package PruebasLeonardo;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 
 public class Ventana extends JFrame{
@@ -20,25 +23,64 @@ public class Ventana extends JFrame{
         this.getContentPane().add(panel); // Agrega el panel a la ventana
 
         AreaIdentificacion();
+        etiquetaAreaIden();
         AreaSeparacion();
+        etiquetaAreaSep();
+        Btn_Select_File();
         AreaArchivoASM();
+        etiquetaAreaAASM();
     }
 
+    private void etiquetaAreaAASM(){
+        JLabel etiquetaAASM = new JLabel();
+        etiquetaAASM.setText("ARCHIVO ASM ORIGINAL");
+        etiquetaAASM.setBounds(150,10,150,50);
+        panel.add(etiquetaAASM);
+    }
     private void AreaArchivoASM (){
         JTextArea archivoASM = new JTextArea();
-        archivoASM.setBounds(20,20,400,500);
+        archivoASM.setBounds(20,80,400,500);
         panel.add(archivoASM);
+    }
+
+    private void Btn_Select_File (){
+        JButton btnSelectFile = new JButton("Seleccionar archivo");
+        btnSelectFile.setBounds(150,650,150,30);
+        panel.add(btnSelectFile);
+
+        // Agregamos el evento que va a abrir la ventana de selección de archivos
+        ActionListener accionBoton = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+        btnSelectFile.addActionListener(accionBoton);
+    }
+
+    private void etiquetaAreaSep(){
+        JLabel etiquetaASep = new JLabel();
+        etiquetaASep.setText("SEPARACION DE ELEMENTOS");
+        etiquetaASep.setBounds(690,10,180,50);
+        panel.add(etiquetaASep);
     }
 
     private void AreaSeparacion (){
         JTextArea AreaSeparacion = new JTextArea();
-        AreaSeparacion.setBounds(500,20,400,500);
+        AreaSeparacion.setBounds(580,80,400,500);
         panel.add(AreaSeparacion);
+    }
+
+    private void etiquetaAreaIden(){
+        JLabel etiquetaAIden = new JLabel();
+        etiquetaAIden.setText("IDENTIFICACION DE ELEMENTOS");
+        etiquetaAIden.setBounds(1230,10,200,50);
+        panel.add(etiquetaAIden);
     }
 
     private void AreaIdentificacion (){
         JTextArea AreaIdentificacion = new JTextArea();
-        AreaIdentificacion.setBounds(1000,20,400,500);
+        AreaIdentificacion.setBounds(1120,80,400,500);
         panel.add(AreaIdentificacion);
     }
 }
