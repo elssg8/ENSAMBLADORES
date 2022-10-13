e154c69205bca2d6642659bdd23826819602c853
Jose Rios <roraj77@hotmail.com>
2022-10-13 16:12:01 -0500

    Agregar Analizador

---
+++ src/modelo/Analizador.java
@@ -0,0 +1,120 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public class Analizador {
+    private String txt;
+    private char txtCompleto [];
+    private ArrayList<Token> palabras;
+    private ArrayList<String> lineas;
+    private String resultado;
+
+
+
+
+    public Analizador(String texto) {
+        this.txt = texto;
+    }
+
+    public String analizaArchivo() {
+
+        txtCompleto = txt.toCharArray();
+
+        String linea = "";
+        this.lineas = new ArrayList<String>();
+
+        String palabra;
+
+        palabra = "";
+        this.palabras =  new ArrayList<Token>();
+
+        char[] aux = {};
+
+        //DIVIDIR EN LINEAS
+        for(int i =0; i<txtCompleto.length; i++) {
+            if(txtCompleto[i]=='\n') {
+                lineas.add(linea);
+                linea = "";
+                continue;
+            }else {
+                linea = linea + txtCompleto[i];
+            }
+        }// Fin divide lineas
+
+        for(int i = 0; i<lineas.size();i++) {
+            if(palabra !="") {
+                palabras.add(new Token(palabra));
+                palabra = "";
+            }
+
+
+            aux = lineas.get(i).toCharArray();
+
+            for(int j =0; j<aux.length; j++) {
+                if(aux[j]==';') {
+                    break;
+                }else {
+                    if(aux[j]!=' ' && aux[j]!=',' && aux[j]!='\n') {
+                        palabra = palabra + aux[j];
+
+
+                        if(palabra.equalsIgnoreCase("STACK")) {
+                            palabra = "STACK SEGMENT";
+                            palabras.add(new Token(palabra));
+                            palabra ="";
+                            break;
+                        }
+
+                        if(palabra.equalsIgnoreCase("DATA")) {
+                            palabra = "DATA SEGMENT";
+                            palabras.add(new Token(palabra));
+                            palabra ="";
+                            break;
+                        }
+
+                        if(palabra.equalsIgnoreCase("CODE")) {
+                            palabra = "CODE SEGMENT";
+                            palabras.add(new Token(palabra));
+                            palabra ="";
+                            break;
+                        }
+
+                        if(aux[j]=='\'') {
+                            for(int k = j; k<aux.length;k++) {
+                                palabra = palabra + aux[k];
+                                if(aux[k]==','|| aux[k]=='\n') {
+                                    if(palabra !="") {
+                                        palabras.add(new Token(palabra));
+                                        palabra = "";
+                                    }
+                                    continue;
+                                }
+                            }
+                            if(palabra !="") {
+                                palabras.add(new Token(palabra));
+                                palabra = "";
+                            }							break;
+                        }
+
+                    }
+                    if(aux[j]==' ' || aux[j]==','|| aux[j]=='\n') {
+                        if(palabra !="") {
+                            palabras.add(new Token(palabra));
+                            palabra = "";
+                        }
+                    }
+                }
+            }
+        }
+
+
+        resultado = "";
+        for(int j =0; j<palabras.size(); j++) {
+            resultado = resultado + palabras.get(j).toString();
+        }
+
+        return resultado;
+    }
+
+
+}
